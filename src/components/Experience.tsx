2f31704b61501a54496815122aeb528f456ff377
Flamur Maliqi <contact@flamur-maliqi.de>
2025-03-18 04:41:09 +0100

    icons

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -11,7 +11,7 @@ const experiences = [
     description: [
       "Incoming"
     ],
-    icon: "/lovable-uploads/4fe11f14-6c48-4cf0-b105-1921fa1a64fd.png"
+    icon: "/lovable-uploads/aws-logo.png"
   },
   {
     id: 2,
@@ -37,7 +37,7 @@ const experiences = [
       "Adapted the test framework for easy deployment on multiple OpenShift clusters",
       "Developed a prototype to generate C++ code from a REST interface (Apache Iceberg, IBM Data Gate)"
     ],
-    icon: "/lovable-uploads/57b6919d-70fa-4730-8c00-9f7715e58396.png"
+    icon: "/lovable-uploads/ibm-logo.png"
   },
   {
     id: 4,
@@ -50,7 +50,7 @@ const experiences = [
       "Implemented real-time control logic in C for embedded systems, ensuring performance and accuracy in critical functions like engine management",
       "Created Python tools to automate development processes, boosting productivity and accuracy in data analysis and system integration"
     ],
-    icon: "/lovable-uploads/3e21a508-7437-4278-b293-c8787a8a17b6.png"
+    icon: "/lovable-uploads/audi-logo.png"
   },
   {
     id: 5,
